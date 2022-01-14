f273e117ad8692648d48f8a176564283f5422a33
sd-kant <dejesusscott5@gmail.com>
2022-01-14 12:57:28 +0100

    show confirm modal when click logout button

--- src/views/components/LogoutButton.js
+++ src/views/components/LogoutButton.js
@@ -1,16 +1,30 @@
-import React from "react";
+import React, {useState} from "react";
 import {withTranslation} from "react-i18next";
 import logoutIcon from "../../assets/images/logout.svg";
 import {logout} from "../layouts/MainLayout";
 import clsx from "clsx";
 import style from "./LogoutButton.module.scss";
+import ConfirmModalV2 from "./ConfirmModalV2";
 
 const LogoutButton = ({t}) => {
+  const [visiblePopup, setVisiblePopup] = useState(false);
+
   return (
     <div className={clsx(style.Logout)}>
+      <ConfirmModalV2
+        show={visiblePopup}
+        header={t("logout kenzen")}
+        onOk={() => {
+          setVisiblePopup(false);
+          logout();
+        }}
+        onCancel={() => {
+          setVisiblePopup(false);
+        }}
+      />
       <label
         className={clsx(style.LogoutLabel, 'font-input-label d-flex cursor-pointer')}
-        onClick={logout}
+        onClick={() => setVisiblePopup(true)}
       >
         <img src={logoutIcon} alt="upload icon"/>
         &nbsp;&nbsp;&nbsp;
