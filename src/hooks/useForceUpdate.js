c966f359215f43309da3e8decc2b881d849c4d9a
sd-kant <dejesusscott5@gmail.com>
2022-03-15 10:13:23 +0100

    fix lint issue

--- src/hooks/useForceUpdate.js
+++ src/hooks/useForceUpdate.js
@@ -1,6 +1,7 @@
 import * as React from "react";
 //create your forceUpdate hook
 function useForceUpdate(){
+  // eslint-disable-next-line no-unused-vars
   const [value, setValue] = React.useState(0); // integer state
   return () => setValue(value => value + 1); // update the state to force render
 }
--- src/views/pages/SelectMode.js
+++ src/views/pages/SelectMode.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import logo from "../../assets/images/logo_light.svg";
-import {useTranslation} from "react-i18next";
 import FormSelectMode from "../partials/su-dashboard/FormSelectMode";
 
 
 const SelectMode = () => {
-  const {t} = useTranslation();
-
   return (
     <div className='form-main'>
       <div className='form-header'>
