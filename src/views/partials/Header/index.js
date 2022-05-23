d3deab640da6090593eff62ec225c36ba3fff950
sd-kant <dejesusscott5@gmail.com>
2022-05-23 12:56:47 +0200

    Increase max number of Users displayed to 100

--- src/views/partials/Header/index.js
+++ src/views/partials/Header/index.js
@@ -271,6 +271,7 @@ const Header = (
                         <option value={10}>10</option>
                         <option value={25}>25</option>
                         <option value={50}>50</option>
+                        <option value={100}>100</option>
                       </select>
                     </div>
                   </React.Fragment> : null
