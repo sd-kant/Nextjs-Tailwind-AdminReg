90813557b6c8f90bedbd0c2303befaa10878884b
sd-kant <dejesusscott5@gmail.com>
2022-06-17 09:31:49 +0200

    update version

--- src/constant/index.js
+++ src/constant/index.js
@@ -5,7 +5,7 @@ export const USER_TYPE_ORG_ADMIN = "OrgAdmin";
 export const USER_TYPE_TEAM_ADMIN = "TeamAdmin";
 export const USER_TYPE_OPERATOR = "Operator";
 
-export const CURRENT_VERSION = "3.8.8";
+export const CURRENT_VERSION = "3.8.9";
 export const AVAILABLE_COUNTRIES = [
   {
     "value": "AU",
