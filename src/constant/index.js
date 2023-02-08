171fdf6917b92d1027d875f114e1d0ea58c79414
sd-kant <dejesusscott5@gmail.com>
2023-02-08 15:18:51 +0900

    Update version to 3.9.28

--- src/constant/index.js
+++ src/constant/index.js
@@ -5,7 +5,7 @@ export const USER_TYPE_ORG_ADMIN = "OrgAdmin";
 export const USER_TYPE_TEAM_ADMIN = "TeamAdmin";
 export const USER_TYPE_OPERATOR = "Operator";
 
-export const CURRENT_VERSION = "3.9.27";
+export const CURRENT_VERSION = "3.9.28";
 export const EXPORT_OPTIONS = [
   {
     label: 'CSV',
