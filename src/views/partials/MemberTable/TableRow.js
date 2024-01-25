a3a69df79d7cb8c6ba7b8d3c2bcae13179d026b5
sd-kant <dejesusscott5@gmail.com>
2024-01-25 23:20:52 +0800

    Fix multiple devices for a user in team dashboard

--- src/views/partials/MemberTable/TableRow.js
+++ src/views/partials/MemberTable/TableRow.js
@@ -9,31 +9,39 @@ import avatar from '../../../assets/images/logo_round.png';
 import TableCell from './TableCell';
 import { DEVICE_CONNECTION_STATUS } from '../../../constant';
 import { hasStatusValue } from '../../../utils';
+import { sortMembers } from 'utils/dashboard';
 
 const TableRow = ({ member, visibleColumns, columnsMap }) => {
   const { width } = useWidthContext();
   const checkboxSize = React.useMemo(() => (width < 768 ? 'sm' : 'md'), [width]);
-  const { setVisibleMemberModal, setMember, hideCbtHR } = useDashboardContext();
+  const { setVisibleMemberModal, setMember, hideCbtHR, filter } = useDashboardContext();
   const { selectedMembers, setSelectedMembers } = useMembersContextV2();
   const index = selectedMembers?.findIndex((it) => it.userId === member.userId);
   const checked = index !== -1;
-  const {
-    alertObj: { value: alertValue },
-    connectionObj: { value: connectionValue }
-  } = member;
-  let badgeColorStyle = null;
-  if (connectionValue?.toString() === '3') {
-    if (['1', '2'].includes(alertValue?.toString())) {
-      badgeColorStyle = style.Red;
-    } else {
-      badgeColorStyle = style.Green;
+  // const {
+  //   alertObj: { value: alertValue },
+  //   connectionObj: { value: connectionValue }
+  // } = member;
+
+  const getBadgeColorStyle = React.useCallback((_connectionValue, _alertValue) => {
+    let badgeColorStyle = null;
+    if (_connectionValue?.toString() === '3') {
+      if (['1', '2'].includes(_alertValue?.toString())) {
+        badgeColorStyle = style.Red;
+      } else {
+        badgeColorStyle = style.Green;
+      }
+    } else if (['4', '7'].includes(_connectionValue?.toString())) {
+      badgeColorStyle = style.Yellow;
     }
-  } else if (['4', '7'].includes(connectionValue?.toString())) {
-    badgeColorStyle = style.Yellow;
-  }
-  const userNameGray = hasStatusValue(connectionValue, [DEVICE_CONNECTION_STATUS.NEVER_CONNECTION])
-    ? style.NoConnection
-    : null;
+    return badgeColorStyle;
+  }, []);
+
+  const getUserNameGray = (_connectionValue) => {
+    return hasStatusValue(_connectionValue, [DEVICE_CONNECTION_STATUS.NEVER_CONNECTION])
+      ? style.NoConnection
+      : null;
+  };
 
   const handleSetChecked = React.useCallback(() => {
     if (checked) {
@@ -44,72 +52,76 @@ const TableRow = ({ member, visibleColumns, columnsMap }) => {
       setSelectedMembers([...selectedMembers, member]);
     }
   }, [checked, index, member, selectedMembers, setSelectedMembers]);
-  const hasOtherDevices = member?.others?.length > 0;
+
+  const members = React.useMemo(() => {
+    let temp = [{ ...member, hasOtherDevices: member?.others?.length > 0, others: undefined }];
+    if (member?.others?.length > 0) {
+      member?.others?.forEach((element) => {
+        temp.push({
+          ...element,
+          hasOtherDevices: false
+        });
+      });
+      return sortMembers({ arrOrigin: temp, filter });
+    } else return temp;
+  }, [member, filter]);
+
   return (
     <>
-      <tr
-        className={clsx(style.TableRow)}
-        key={`member-${member.userId}`}
-        onClick={() => {
-          setVisibleMemberModal(true);
-          setMember(member);
-        }}>
-        <td className={clsx(style.TableCell, userNameGray, hasOtherDevices ? style.BorderB0 : '')}>
-          <div className={clsx(style.InnerWrapper)}>
-            <div className={clsx(style.Badge, badgeColorStyle)} />
-            <div style={{ marginTop: '20px' }}>
-              <Checkbox size={checkboxSize} checked={checked} setChecked={handleSetChecked} />
+      {members?.map((subMember, idx) => (
+        <tr
+          className={clsx(style.TableRow)}
+          key={`member-${member.userId}-${idx}`}
+          onClick={() => {
+            setVisibleMemberModal(true);
+            setMember(subMember);
+          }}>
+          <td
+            className={clsx(
+              style.TableCell,
+              getUserNameGray(subMember?.connectionObj?.value),
+              subMember?.hasOtherDevices ? style.BorderB0 : idx > 0 ? style.BorderT0 : ''
+            )}>
+            <div className={clsx(style.InnerWrapper)}>
+              <div
+                className={clsx(
+                  style.Badge,
+                  getBadgeColorStyle(subMember?.connectionObj?.value, subMember?.alertObj?.value)
+                )}
+              />
+              {idx === 0 && (
+                <>
+                  <div style={{ marginTop: '20px' }}>
+                    <Checkbox size={checkboxSize} checked={checked} setChecked={handleSetChecked} />
+                  </div>
+                  <img src={avatar} className={clsx(style.Avatar)} alt="avatar" />
+                  <div style={{ textAlign: 'left' }}>
+                    <div>
+                      <span>{member['firstName']}</span>
+                    </div>
+                    <div>
+                      <span>{member['lastName']}</span>
+                    </div>
+                  </div>
+                </>
+              )}
             </div>
-            <img src={avatar} className={clsx(style.Avatar)} alt="avatar" />
-            <div style={{ textAlign: 'left' }}>
-              <div>
-                <span>{member['firstName']}</span>
-              </div>
-              <div>
-                <span>{member['lastName']}</span>
-              </div>
-            </div>
-          </div>
-        </td>
-        {Object.keys(columnsMap).map((header, index) =>
-          visibleColumns.includes(header) ? (
-            <TableCell
-              value={header}
-              key={`cell-${member.userId}-${index}`}
-              member={member}
-              hideCbtHR={hideCbtHR}
-            />
-          ) : null
-        )}
-        {visibleColumns?.length < Object.keys(columnsMap)?.length + 1 && (
-          <td className={clsx(style.TableCell)} />
-        )}
-      </tr>
-      {hasOtherDevices &&
-        member?.others?.map((subMember, idx) => (
-          <tr
-            className={clsx(style.TableRow)}
-            key={`member-${member.userId}-${idx}`}
-            onClick={() => {
-              setVisibleMemberModal(true);
-              setMember(subMember);
-            }}>
-            <td className={clsx(style.TableCell, userNameGray, style.BorderT0)}></td>
-            {Object.keys(columnsMap).map((header, index) =>
-              visibleColumns.includes(header) ? (
-                <TableCell
-                  value={header}
-                  key={`cell-${member.userId}-${index}`}
-                  member={subMember}
-                  hideCbtHR={hideCbtHR}
-                />
-              ) : null
-            )}
-            {visibleColumns?.length < Object.keys(columnsMap)?.length + 1 && (
-              <td className={clsx(style.TableCell)} />
-            )}
-          </tr>
-        ))}
+          </td>
+          {Object.keys(columnsMap).map((header, index) =>
+            visibleColumns.includes(header) ? (
+              <TableCell
+                value={header}
+                key={`cell-${member.userId}-${index}`}
+                member={subMember}
+                hideCbtHR={hideCbtHR}
+              />
+            ) : null
+          )}
+          {visibleColumns?.length < Object.keys(columnsMap)?.length + 1 && (
+            <td className={clsx(style.TableCell)} />
+          )}
+        </tr>
+      ))}
     </>
   );
 };
