b197cf83e95ce45ee7319e310bff83087561fa54
sd-kant <dejesusscott5@gmail.com>
2024-07-17 01:43:03 +0800

    add medical questionnaire property on company setting page

--- src/constant/index.js
+++ src/constant/index.js
@@ -258,6 +258,21 @@ export const hideCbtHROptions = [
   }
 ];
 
+export const medicalQuestionnaireOptions = [
+  {
+    value: 1,
+    title: i18n.t('yes')
+  },
+  {
+    value: 0,
+    title: i18n.t('no')
+  },
+  {
+    value: 2,
+    title: i18n.t('optional')
+  }
+];
+
 export const IMPERIAL = 'imperial';
 export const METRIC = 'metric';
 export const MALE = 'male';
